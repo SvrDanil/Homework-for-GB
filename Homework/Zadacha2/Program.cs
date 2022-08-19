45abe4f2735021a200add0917d5f156192b9b916
Danil Savrasov <Danukan74@gmail.com>
2022-08-19 17:23:40 +0500

    Complete

---
+++ Homework/Zadacha2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите 1 число");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b=int.Parse(Console.ReadLine());
+if (a>b)
+    {Console.WriteLine($"{a} Больше");}   
+else
+    {Console.WriteLine($"{b} Больше");}   
---
+++ Homework/Zadacha4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите 1 число");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int c=int.Parse(Console.ReadLine());
+int max=a;
+if (max<b){max=b;}
+else if (max<c){max=c;}
+Console.WriteLine($"Самое большое ={max}");
+  
+  
