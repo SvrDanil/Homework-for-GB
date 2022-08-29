eb8efb6dd8329a611a4a6654ba77effa2e24e7e6
Danil Savrasov <Danukan74@gmail.com>
2022-08-29 18:57:43 +0500

    Homework_3

---
+++ Homework_3/Task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Строки и массивы использовать нельзя!
+
+Console.WriteLine(" Введите цифру дня недели");
+int num = int.Parse(Console.ReadLine());
+int pol=0;
+int numClone=num;
+while(num>0)
+{
+    int digit = num%10;
+    pol=pol*10+digit;
+    num=num/10;
+}
+if (numClone==pol)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
---
+++ Homework_3/Task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите x первой точки");
+int x1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y первой точки");
+int y1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z первой точки");
+int z1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x второй точки");
+int x2= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y второй точки");
+int y2= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z второй точки");
+int z2= int.Parse(Console.ReadLine());
+
+int sqr1=(x1-x2)*(x1-x2);
+int sqr2=(y1-y2)*(y1-y2);
+int sqr3 =( z1-z2)*(z1-z2);
+
+int sum = sqr1+sqr2+sqr3;
+double result = Math.Sqrt(sum);
+Console.WriteLine($"Расстояние = {result:f2}");
