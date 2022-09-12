a6893a48390d5ca46ac3b1fb673b3b4287f4ee96
Danil Savrasov <Danukan74@gmail.com>
2022-09-12 21:01:35 +0500

    Homework_7

---
+++ Homework_6/Task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[]  numbers(int count)
+{   
+    int [] arr= new int[count];
+    for (int i = 0;i<count;i++)
+    {
+        Console.WriteLine("Введите число");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr ;
+}
+
+int counter(int[] arr1)
+{
+    int count=0;
+    for (int i = 0;i<arr1.Length;i++)
+    {
+        if (arr1[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+
+}
+Console.WriteLine("Введите Число M");
+
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Чисел больше нуля :{counter(numbers(M))}");
---
+++ Homework_6/Task_43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void crossPoint(double[,] matr)
+{   
+   
+    if (matr[0,1]==matr[1,1])
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+         double x=(matr[1,0] - matr[0,0])/(matr[0,1] - matr[1,1]);
+         double y= (matr[0,1]*x) + matr[0,0];
+         Console.WriteLine($"x={x} y={y}");
+    }   
+
+}
+double[,] arr =  new double[2,2];
+Console.Write("Введите b1: ");
+arr[0,0]= double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+arr[0,1]= double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+arr[1,0]= double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+arr[1,1]= double.Parse(Console.ReadLine());
+
+crossPoint(arr);
