a869a714f45c92ce0d9708765b7207628a4f9da6
Danil Savrasov <Danukan74@gmail.com>
2022-09-21 16:28:57 +0500

    Homework_9

---
+++ Homework_9/Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int SumCountRec(int m,int n) 
+{
+
+    if(m==n) return m;
+    else return m+(SumCountRec(m+1,n));
+    
+}
+Console.WriteLine("Ведите M");
+int M=int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите N");
+int N=int.Parse(Console.ReadLine());
+Console.WriteLine(SumCountRec(M,N));
---
+++ Homework_9/Task_68/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int AkkermanFunc(int m,int n)
+{
+    int answer;
+    if (m == 0) answer = n + 1;
+    else if (n == 0) answer = AkkermanFunc(m - 1, 1);
+    else answer = AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+    return answer;
+}
+Console.WriteLine("Ведите m");
+int M=int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите n");
+int N=int.Parse(Console.ReadLine());
+Console.WriteLine(AkkermanFunc(M,N));
